2de374961a370d7f328f6f6f528cfdfe3aae1e33
lobor <lionel.bertrand@ymail.com>
2016-04-25 19:16:29 +0200

    test npm

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-require('./dist/js/script.js');
+import { IA } from './src/ia/ia.js';
+
+var ElectronSettings = require('electron-settings');
+window.moment = require('moment');
+require('moment-range');
+require('moment/locale/fr');
+
+
+class Rasp{
+  constructor(){
+    console.log('Init RaspApp');
+		this.ia = new IA();
+  }
+}
+
+
+
+let settings = new ElectronSettings({
+  configDirPath: './'
+});
+
+if (!settings.get('lang')) {
+  settings.set('lang', 'fr');
+}
+
+if (!settings.get('firstLoad')) {
+  settings.set('firstLoad', new Date().getTime());
+}
+
+if (!settings.get('name')) {
+  settings.set('name', 'jaspi');
+}
+
+window.Settings = settings.get();
+
+window.moment.locale(settings.get('lang'));
+
+let app = new Rasp();
